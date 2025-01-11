b842ba47cec5d67e63e19c0356242dea53937e27
Mel-Rocha <rochamel73@gmail.com>
2025-01-11 20:06:55 -0300

    validação básica nas features categóricas que tem poucas opções de escolha

--- apps/car/routes.py
+++ apps/car/routes.py
@@ -4,16 +4,6 @@ from apps.car.schemas import Car
 
 router = APIRouter()
 
-@router.get("/car", response_model=Car)
-async def get_car():
-    return {
-        "year_of_reference": 2023,
-        "month_of_reference": 10,
-        "engine_size": 2.0,
-        "year_model": 2023,
-        "age_years": 0,
-        "fuel": "Gasoline",
-        "gear": "Automatic",
-        "brand": "Toyota",
-        "model": "Corolla"
-    }
+@router.post("/predict", response_model=Car)
+async def create_car(car: Car):
+    return car
--- apps/car/schemas.py
+++ apps/car/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Car(BaseModel):
@@ -11,3 +11,17 @@ class Car(BaseModel):
     gear: str
     brand: str
     model: str
+
+    @validator('fuel')
+    def validate_fuel(cls, value):
+        allowed_fuels = ['Alcohol', 'Diesel', 'Gasoline']
+        if value not in allowed_fuels:
+            raise ValueError(f'Invalid fuel type: {value}. Allowed values are: {allowed_fuels}')
+        return value
+
+    @validator('gear')
+    def validate_gear(cls, value):
+        allowed_gears = ['automatic', 'manual']
+        if value not in allowed_gears:
+            raise ValueError(f'Invalid gear type: {value}. Allowed values are: {allowed_gears}')
+        return value
