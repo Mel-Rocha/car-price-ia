1ae1a369def9bcfd64fe9fd9a247924d7e7b6de3
Mel-Rocha <rochamel73@gmail.com>
2025-01-26 17:25:38 -0300

    Ajustei para usar a classe AppState no evento @app.on_event('startup') async def startup_event() e defini corretamente o tipo esperado em AppState, resolvendo os avisos do PyCharm.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import joblib
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 from apps.car import routes as car_router
 from apps.docs import routes as docs_router
@@ -11,9 +13,9 @@ from settings import config, MODEL_PATH, NORMALIZER_PATH, TRANSFORMER_PATH, X_TE
 
 
 class AppState:
-    NORMALIZER: any
-    TRANSFORMER: any
-    MODEL: any
+    NORMALIZER: StandardScaler
+    TRANSFORMER: OneHotEncoder
+    MODEL: RandomForestRegressor
     X_test: pd.DataFrame
     ORIGINAL_DF: pd.DataFrame
 
@@ -51,17 +53,12 @@ app.openapi = lambda: custom_openapi(app)
 
 @app.on_event('startup')
 async def startup_event():
+    app.state = AppState()
+
     app.state.NORMALIZER = joblib.load(NORMALIZER_PATH)
-
-    # Carregar o transformador categórico
     app.state.TRANSFORMER = joblib.load(TRANSFORMER_PATH)
-
-    # Carregar o modelo treinado
     app.state.MODEL = joblib.load(MODEL_PATH)
-
-    # Carregar o conjunto de dados X_test
     app.state.X_test = pd.read_csv(X_TEST_PATH)
-
-    # Carregar o DataFrame original usado para calcular médias dinâmicas
     app.state.ORIGINAL_DF = pd.read_csv(ORIGINAL_DF_PATH)
+
     config.load_valid_brands('data/valid_brands.csv')
